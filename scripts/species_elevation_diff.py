6178e95e07c5860b1f713746abb2949f695d5894
Luke Guild <luke.guild@gmail.com>
2023-07-07 10:48:53 -0400

    diff bugfix, add wide + pres

--- scripts/species_elevation_diff.py
+++ scripts/species_elevation_diff.py
@@ -8,24 +8,34 @@ def midpoint(elevation_bracket):
     if elevation_bracket == 'N/A':
         return None
     low, _ = map(int, elevation_bracket.split('-'))
-    return low + 50  # Get midpoint
+    return low + 50  # get midpoint
 
 norm_thresholds = ['0.01', '0.05']
 datasets = ['a.10.5', 'b.5.0', 'c.0.-5', 'd.-5.-10', 'e.-10.-15', 'f.-15.-20', 'g.-20.-25']
 
-species_data = {}
 for threshold in norm_thresholds:
     os.chdir(IN_LOC)
+
+    species_data = {}
+    region_presence = {}
+    
     for dataset in datasets:
         filename = f'species_min_max_elevation/{threshold}/species_min_max_elevation.{threshold}.{dataset}.csv'
         with open(filename, 'r') as f:
             reader = csv.reader(f)
-            next(reader)  # Skip header
+            next(reader)  # skip header
             for row in reader:
                 species = row[0]
                 min_bracket = midpoint(row[1])
                 max_bracket = midpoint(row[2])
+                # if present in at least 1 region
                 if min_bracket is not None and max_bracket is not None:
+                    # tally region presence for this species
+                    if dataset not in region_presence:
+                        region_presence[dataset] = 1
+                    else:
+                        region_presence[dataset] += 1
+                    # then calculate range/diff
                     value = max_bracket - min_bracket
                     if species not in species_data:
                         species_data[species] = {}
@@ -39,3 +49,22 @@ for threshold in norm_thresholds:
         for species, data in species_data.items():
             row = [species] + [data.get(dataset, 'N/A') for dataset in datasets]
             writer.writerow(row)
+
+    # another output csv with only "wide" ranging species that occur in at least 3 lat bands
+    output_csv = f'species_elevation_diff_wide.{threshold}.csv'
+    with open(output_csv, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['species'] + datasets)
+        for species, data in species_data.items():
+            if (len(data.values()) < 3):
+                continue
+            row = [species] + [data.get(dataset, 'N/A') for dataset in datasets]
+            writer.writerow(row)
+
+    # species presence (after threshhold)
+    output_csv = f'species_elevation_presence.{threshold}.csv'
+    with open(output_csv, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['region', 'count'])
+        for region, count in region_presence.items():
+            writer.writerow([region, count])
